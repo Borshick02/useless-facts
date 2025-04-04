ee2d7ffa0e147ff1d35a1acb89c3ed52b74b498e
Angelina Zolotova <aazolotova_1@edu.hse.ru>
2025-04-05 00:11:32 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// Функция для получения случайного факта
+const fetchFact = async (endpoint) => {
+    const response = await fetch(`https://uselessfacts.jsph.pl/api/v2/facts/${endpoint}?language=en`);
+    const data = await response.json();
+    document.getElementById('fact').textContent = data.text;
+  };
+  
+  // Привязываем события к кнопкам
+  document.getElementById('random-fact-btn').addEventListener('click', () => fetchFact('random'));
+  document.getElementById('fact-of-the-day-btn').addEventListener('click', () => fetchFact('today'));
+  
